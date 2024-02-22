a1e129ab18443fda59e04014994116276448cc7f
James Hawk <jph94880@gmail.com>
2024-02-22 14:21:07 -0600

    Adds weatherAPI cavriable to fetch request.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import CurrentDateAndTimeCard from './CurrentDateAndTimeCard';
 import react, { useState, useEffect } from 'react'
 
 const API = "http://127.0.0.1:5555/forecast"
+const weatherAPI = "https://api.weather.gov/points/38.7192,-94.4586"
 
 function App() {
   const [weather, setWeather] = useState(null)
 
   useEffect(() => {
-    const fetchWeather = () => fetch(API).then(r => r.json())
+    const fetchWeather = () => fetch(weatherAPI).then(r => r.json())
 
     async function startFetching() {
       const weatherData = await fetchWeather()
