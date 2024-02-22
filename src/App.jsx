0c05b42ee393c74c3579f303ff89d885858663ea
James Hawk <jph94880@gmail.com>
2024-02-22 13:44:13 -0600

    Successfully fetches weatherData from db.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import CurrentWeatherCard from './CurrentWeatherCard';
 import CurrentDateAndTimeCard from './CurrentDateAndTimeCard';
+import react, { useState, useEffect } from 'react'
+
+const API = "http://127.0.0.1:5555/forecast"
 
 function App() {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const fetchWeather = () => fetch(API).then(r => r.json())
+
+    async function startFetching() {
+      const weatherData = await fetchWeather()
+      console.log(weatherData)
+      setWeather(weatherData)
+    }
+    startFetching();
+    
+  }, [])
+
   return (
     <>
     <CurrentDateAndTimeCard/>
